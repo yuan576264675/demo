a13130667353bead88d43c86948cc257c369f2b3
yuan576264675 <yuan576264675@163.com>
2017-12-26 17:40:15 +0800

    新增部分内容

---
+++ src/main/java/cn/yuan576264675/demo/entity/enums/BaseEnum.java
@@ -0,0 +1,32 @@
+/**   
+ * Copyright © 2017 eSunny Info. Tech Ltd. All rights reserved.
+ * 
+ * 功能描述：
+ * @Package: cn.yuan576264675.demo.entity.enums 
+ * @author: chenjiangfeng   
+ * @date: 2017年12月26日 下午2:30:00 
+ */
+package cn.yuan576264675.demo.entity.enums;
+
+/**   
+* Copyright: Copyright (c) 2017 yuan
+* 
+* @ClassName: BaseEnum.java
+* @Description: 该类的功能描述
+*
+* @version: v1.0.0
+* @author: yuan
+* @date: 2017年12月26日 下午2:30:00 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2017年12月26日     chenjiangfeng     v1.0.0               修改原因
+*/
+public interface BaseEnum {
+
+	int getIntKey();
+	
+	String getStrKey();
+	
+}
---
+++ src/main/java/cn/yuan576264675/demo/entity/enums/EnumUtils.java
@@ -0,0 +1,30 @@
+/**   
+ * Copyright © 2017 eSunny Info. Tech Ltd. All rights reserved.
+ * 
+ * 功能描述：
+ * @Package: cn.yuan576264675.demo.entity.enums 
+ * @author: chenjiangfeng   
+ * @date: 2017年12月26日 下午2:31:33 
+ */
+package cn.yuan576264675.demo.entity.enums;
+
+import java.util.Map;
+
+/**   
+* Copyright: Copyright (c) 2017 yuan
+* 
+* @ClassName: EnumUtils.java
+* @Description: 该类的功能描述
+*
+* @version: v1.0.0
+* @author: yuan
+* @date: 2017年12月26日 下午2:31:33 
+*
+* Modification History:
+* Date         Author          Version            Description
+*---------------------------------------------------------*
+* 2017年12月26日     chenjiangfeng     v1.0.0               修改原因
+*/
+public class EnumUtils {
+
+}
